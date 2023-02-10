dab33bace3b9a3cfba116d2b99c6d58a8eb96ee8
unknown <catalina.ordenes@mail.udp.cl>
2023-02-10 11:38:11 -0300

    Vinculación de Firebase, primeras importaciones para logearse con Google

---
+++ Sticky-list/Firebase/Dependencias.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+
+initializeApp({
+    credential: _credential.cert(serviceAccount)
+});
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASlgSQpq4GZs0y46rz7nrncV_CE0wiMro",
+    authDomain: "stickylist-dev001.firebaseapp.com",
+    projectId: "stickylist-dev001",
+    storageBucket: "stickylist-dev001.appspot.com",
+    messagingSenderId: "405206344603",
+    appId: "1:405206344603:web:76fdcf44ddcdc8c16f373e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export const registroGoogle = () => {
+    return signInWithPopup(auth, provider);
+}
