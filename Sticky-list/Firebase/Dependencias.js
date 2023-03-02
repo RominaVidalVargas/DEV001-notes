ccd8607782b044c0dedd0cfef30a01be418f312d
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2023-03-02 18:18:50 -0300

    css y responsive de notas

--- Sticky-list/Firebase/Dependencias.js
+++ Sticky-list/Firebase/Dependencias.js
@@ -1,15 +1,22 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { getFirestore, doc, setDoc, collection, onSnapshot, addDoc } from "firebase/firestore";
+import {
+  getAuth, GoogleAuthProvider,
+  signInWithPopup, signOut
+}
+  from 'firebase/auth';
+import {
+  getFirestore, doc, setDoc,
+  collection, onSnapshot, addDoc, getDocs
+} from 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyASlgSQpq4GZs0y46rz7nrncV_CE0wiMro",
-    authDomain: "stickylist-dev001.firebaseapp.com",
-    projectId: "stickylist-dev001",
-    storageBucket: "stickylist-dev001.appspot.com",
-    messagingSenderId: "405206344603",
-    appId: "1:405206344603:web:76fdcf44ddcdc8c16f373e"
+  apiKey: 'AIzaSyASlgSQpq4GZs0y46rz7nrncV_CE0wiMro',
+  authDomain: "stickylist-dev001.firebaseapp.com",
+  projectId: "stickylist-dev001",
+  storageBucket: "stickylist-dev001.appspot.com",
+  messagingSenderId: "405206344603",
+  appId: "1:405206344603:web:76fdcf44ddcdc8c16f373e"
 };
 
 // Initialize Firebase
@@ -20,20 +27,25 @@ const db = getFirestore(app);
 
 
 export const registroGoogle = () => {
-    return signInWithPopup(auth, provider);
+  return signInWithPopup(auth, provider);
 }
 
-export const salirDeCuenta = async() => {
-    await signOut(auth);
+export const salirDeCuenta = async () => {
+  await signOut(auth);
 };
 
-export const lectorDatos = () => {
-    return doc(collection(db, 'sticky'))
+export const lectorDatos = async () => {
+  const querySnapshot = await getDocs(collection(db, 'stickypost'));
+  const documentoRenderizar = [];
+  querySnapshot.forEach((doc) => {
+    documentoRenderizar.push({ ...doc.data(), id: doc.id });
+    // doc.data() is never undefined for query doc snapshots
+  });
+  return documentoRenderizar
 };
-
 
 export const crearDocumento = (objetoInput) => {
-    return addDoc(collection(db, "stickypost"), objetoInput)
+  return addDoc(collection(db, 'stickypost'), objetoInput);
 };
 
 
